562b4749a5fdd4eeb1ffc802ec80e5339323f126
Kobby <kwabenaacheampong812@gmail.com>
2025-08-09 03:00:53 +0000

    updated website, removed tawk chat AI bot, modified the skills section, added some graphic design works

--- script.js
+++ script.js
@@ -40,12 +40,12 @@ themeIcon.addEventListener('click', () => {
 
 // tawk chatbot
 
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/62b42aa97b967b11799617ef/1g67sda0r';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
+// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+// (function () {
+//     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+//     s1.async = true;
+//     s1.src = 'https://embed.tawk.to/62b42aa97b967b11799617ef/1g67sda0r';
+//     s1.charset = 'UTF-8';
+//     s1.setAttribute('crossorigin', '*');
+//     s0.parentNode.insertBefore(s1, s0);
+// })();
